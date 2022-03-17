b65c829c907ee33f2a4bfdbd56bace6b6bdebf4f
Chrisnewgear <chrisnewgear2011@gmail.com>
2022-03-17 16:16:42 -0500

    CONTACT FORM DONE / TOGGLE MENU FOR MOBILE IN PROGRESS

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+window.sr = ScrollReveal();
+sr.reveal('.navigation', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.logo', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.content', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('.btn', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+});
