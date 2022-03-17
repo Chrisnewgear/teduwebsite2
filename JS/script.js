2c63277e406b523cef8f6b7b0cb132b78a3e5fb2
Chrisnewgear <chrisnewgear2011@gmail.com>
2022-03-17 17:49:00 -0500

    RESPONSIVE ON NOSOTROS IS WORKING/ALL OTHER SECTIONS ARE REMAIN

--- JS/script.js
+++ JS/script.js
@@ -5,5 +5,7 @@ window.addEventListener('scroll', function(){
 
 function toggleMenu(){
     const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
     menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
 }
